505d037262445c24ba91bab23d54d302a1ce6709
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-11 11:58:31 -0300

    routers login

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -23,5 +23,4 @@ const controller = {
     });
   },
 };
-
 export default controller;
--- server/router/router.js
+++ server/router/router.js
@@ -2,6 +2,14 @@ import express from "express";
 import controller from "../controllers/controller";
 const router = express.Router();
 
+router.post("/register", (req, res) => {
+  res.send("register");
+});
+
+router.post("/login", (req, res) => {
+  res.send("login");
+});
+
 router.use("/room", controller.roomPath);
 
 router.get("/", controller.loginPath);
